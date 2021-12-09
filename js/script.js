01a2a91a46797c63cb780118cb248fd805dafa18
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-12-08 16:42:34 -0800

    add full app

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const btns = document.querySelectorAll('.js-btn');
+const inputs = document.querySelectorAll('.js-input');
+
+const [resetBtn, copyBtn] = btns;
+const [input, output] = inputs;
+
+const mrRobot = function (str) {
+  output.value = str
+    .toUpperCase()
+    .trim()
+    .split(' ')
+    .map((el) =>
+      el
+        .replace(/A/gi, '4')
+        .replace(/E/gi, '3')
+        .replace(/I/gi, '1')
+        .replace(/O/gi, '0')
+        .replace(/S/gi, '5')
+        .replace(/T/gi, '7')
+    )
+    .join(' ');
+};
+
+const resetAll = () => inputs.forEach((input) => (input.value = ''));
+const copy = function () {
+  input.select();
+  input.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(input.value);
+};
+
+input.addEventListener('input', (e) => mrRobot(e.target.value));
+resetBtn.addEventListener('click', resetAll);
+copyBtn.addEventListener('click', copy);
+window.addEventListener('load', resetAll);
